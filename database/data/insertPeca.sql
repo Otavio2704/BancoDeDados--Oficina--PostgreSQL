a19fd54b75c410f3af238cb058c11b6b76a88d4c
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:20:05 -0300

    Add SQL insert statements for PECA table

---
+++ database/data/insertPeca.sql
@@ -0,0 +1,11 @@
+INSERT INTO PECA (nome, descricao, preco_unitario, estoque_atual, estoque_minimo, id_fornecedor) VALUES
+('Óleo Motor 5W30', 'Óleo sintético para motor', 45.00, 50, 10, 1),
+('Filtro de Óleo', 'Filtro de óleo do motor', 25.00, 30, 5, 1),
+('Pastilha de Freio', 'Pastilha de freio dianteira', 120.00, 20, 4, 2),
+('Amortecedor Dianteiro', 'Amortecedor dianteiro esquerdo', 180.00, 15, 2, 2),
+('Vela de Ignição', 'Vela de ignição NGK', 35.00, 40, 8, 3),
+('Filtro de Ar', 'Filtro de ar do motor', 40.00, 25, 5, 1),
+('Correia Dentada', 'Correia dentada do motor', 150.00, 12, 3, 3),
+('Disco de Freio', 'Disco de freio ventilado', 200.00, 10, 2, 2),
+('Fluido de Freio', 'Fluido de freio DOT 4', 20.00, 35, 6, 1),
+('Pneu 185/60R15', 'Pneu aro 15', 280.00, 16, 4, 3);
