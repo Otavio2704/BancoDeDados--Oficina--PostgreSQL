b5ee03a449b787293878f8cf9aedf50d2ef95a08
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:18:33 -0300

    Add SQL insert statements for MECANICO table

---
+++ database/data/insertMecanico.sql
@@ -0,0 +1,6 @@
+INSERT INTO MECANICO (nome, cpf, telefone, especialidade, salario, data_contratacao) VALUES
+('Pedro Mechanic', '111.222.333-44', '(11) 1111-2222', 'Motor', 3500.00, '2020-01-15'),
+('Ana Repair', '222.333.444-55', '(11) 2222-3333', 'Suspensão', 3200.00, '2019-05-20'),
+('Roberto Fix', '333.444.555-66', '(11) 3333-4444', 'Elétrica', 3800.00, '2018-03-10'),
+('Luisa Tools', '444.555.666-77', '(11) 4444-5555', 'Freios', 3300.00, '2021-07-08'),
+('José Motors', '555.666.777-88', '(11) 5555-6666', 'Transmissão', 4000.00, '2017-11-25');
