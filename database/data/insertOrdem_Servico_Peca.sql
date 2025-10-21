934399629d0f3bf760b6c2b9a1b9a8c634c04f71
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:22:45 -0300

    Add SQL insert statements for ORDEM_SERVICO_PECA

---
+++ database/data/insertOrdem_Servico_Peca.sql
@@ -0,0 +1,8 @@
+INSERT INTO ORDEM_SERVICO_PECA (id_ordem, id_peca, quantidade, preco_praticado) VALUES
+(1, 1, 1, 45.00),
+(1, 2, 1, 25.00),
+(4, 3, 1, 120.00),
+(4, 8, 1, 200.00),
+(6, 4, 2, 180.00),
+(7, 5, 4, 35.00),
+(8, 7, 1, 150.00);
