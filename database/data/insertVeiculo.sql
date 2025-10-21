27af0b07ec0e225069bfbba72e279accad90d91f
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:19:14 -0300

    Add SQL insert statements for VEICULO table

---
+++ database/data/insertVeiculo.sql
@@ -0,0 +1,9 @@
+INSERT INTO VEICULO (placa, marca, modelo, ano, cor, id_cliente) VALUES
+('ABC-1234', 'Toyota', 'Corolla', 2020, 'Prata', 1),
+('DEF-5678', 'Honda', 'Civic', 2019, 'Preto', 2),
+('GHI-9012', 'Ford', 'Focus', 2021, 'Branco', 3),
+('JKL-3456', 'Volkswagen', 'Jetta', 2018, 'Azul', 1),
+('MNO-7890', 'Chevrolet', 'Onix', 2022, 'Vermelho', 4),
+('PQR-1357', 'Renault', 'Sandero', 2020, 'Cinza', 5),
+('STU-2468', 'Nissan', 'Versa', 2019, 'Branco', 3),
+('VWX-3691', 'Hyundai', 'HB20', 2021, 'Prata', 2);
