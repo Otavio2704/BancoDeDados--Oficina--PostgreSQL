6840100c39c70f19abc3f3c5cff8c7979aa5b79f
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:44:34 -0300

    Update analysis15.sql

--- database/queries/analysis15.sql
+++ database/queries/analysis15.sql
@@ -1,8 +1,8 @@
 -- Análise de sazonalidade de serviços
 SELECT 
-    YEAR(os.data_entrada) as ano,
-    MONTH(os.data_entrada) as mes,
-    MONTHNAME(os.data_entrada) as nome_mes,
+    EXTRACT(YEAR FROM os.data_entrada) as ano,
+    EXTRACT(MONTH FROM os.data_entrada) as mes,
+    TO_CHAR(os.data_entrada, 'Month') as nome_mes,
     COUNT(os.id_ordem) as total_ordens,
     SUM(os.valor_total) as faturamento_mes,
     AVG(os.valor_total) as ticket_medio,
@@ -10,5 +10,5 @@ SELECT
     ROUND(SUM(os.valor_total) / COUNT(DISTINCT os.id_veiculo), 2) as faturamento_por_veiculo
 FROM ORDEM_SERVICO os
 WHERE os.status = 'FINALIZADA'
-GROUP BY YEAR(os.data_entrada), MONTH(os.data_entrada), MONTHNAME(os.data_entrada)
+GROUP BY EXTRACT(YEAR FROM os.data_entrada), EXTRACT(MONTH FROM os.data_entrada), TO_CHAR(os.data_entrada, 'Month')
 ORDER BY ano DESC, mes DESC;
