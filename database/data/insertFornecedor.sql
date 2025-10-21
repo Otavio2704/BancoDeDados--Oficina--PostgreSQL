b8f458e5403313f4adfb99ea87558146549ee9a3
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:11:32 -0300

    Fix SQL syntax by adding newline at end of file

---
+++ database/data/insertFornecedor.sql
@@ -0,0 +1,4 @@
+INSERT INTO FORNECEDOR (nome, cnpj, telefone, email, endereco) VALUES
+('Auto Peças São Paulo', '12.345.678/0001-90', '(11) 3456-7890', 'contato@autopecassp.com.br', 'Rua das Peças, 123 - São Paulo/SP'),
+('Distribuidora Central', '98.765.432/0001-10', '(11) 2345-6789', 'vendas@distcentral.com.br', 'Av. Central, 456 - São Paulo/SP'),
+('Peças Premium Ltda', '11.222.333/0001-44', '(11) 4567-8901', 'premium@pecaspremium.com.br', 'Rua Premium, 789 - São Paulo/SP');
