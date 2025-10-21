1cba32615177a2a004aa04e63e5c50d4e5a65543
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:36:03 -0300

    Change id_fornecedor type from INT to SERIAL

--- database/schema/tabelaFornecedor.sql
+++ database/schema/tabelaFornecedor.sql
@@ -1,5 +1,5 @@
 CREATE TABLE FORNECEDOR (
-    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+    id_fornecedor SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cnpj VARCHAR(18) UNIQUE NOT NULL,
     telefone VARCHAR(15),
