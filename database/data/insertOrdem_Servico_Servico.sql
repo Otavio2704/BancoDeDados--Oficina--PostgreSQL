a8f347cadaf6717d421d99ede1aaab6a9c234a95
Otavio2704 <otavioaredes62@gmail.com>
2025-10-21 20:20:56 -0300

    Add SQL insert statements for ORDEM_SERVICO_SERVICO

---
+++ database/data/insertOrdem_Servico_Servico.sql
@@ -0,0 +1,10 @@
+INSERT INTO ORDEM_SERVICO_SERVICO (id_ordem, id_servico, quantidade, preco_praticado) VALUES
+(1, 1, 1, 80.00),
+(2, 2, 1, 120.00),
+(2, 3, 1, 100.00),
+(3, 4, 1, 350.00),
+(4, 5, 1, 200.00),
+(5, 6, 1, 150.00),
+(6, 7, 1, 400.00),
+(7, 8, 1, 180.00),
+(8, 4, 1, 350.00);
